e77476e751c7900112d1dd539b3d529cb1548d85
lynnnnnnnn <52003629@qq.com>
2025-11-09 21:03:12 +0800

    feat: ssl

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   server: {
     host: '0.0.0.0',
     port: 3000,
+    https: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
@@ -13,4 +15,9 @@ export default defineConfig({
       },
     },
   },
+  preview: {
+    // 预览服务器会自动继承 server.https 的设置
+    host: '0.0.0.0',
+    port: 4173
+  }
 })
